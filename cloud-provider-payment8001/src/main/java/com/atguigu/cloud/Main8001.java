c77c6f7b1d19d38fc2cdf6dc7d94626492d6ae1c
xiaotuifei <tuixiaofei@icloud.com>
2024-09-04 23:45:36 +0800

    init project

---
+++ cloud-provider-payment8001/src/main/java/com/atguigu/cloud/Main8001.java
@@ -0,0 +1,15 @@
+package com.atguigu.cloud;
+
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author tuixiaofei
+ */
+@SpringBootApplication
+public class Main8001 {
+    public static void main(String[] args) {
+        SpringApplication.run(Main8001.class, args);
+    }
+}
